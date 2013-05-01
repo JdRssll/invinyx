fe37bbd46afd086573e03bd25ad4d4ee9d2f75ef
Jesus Rossell <jdr@jdr-VirtualBox.(none)>
2013-05-01 12:45:52 -0430

    Agregando proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Invinyx::Application.config.secret_token = '80c8d1eabfaa53ba3769d78be3490b50ef971fe0ee9a184d6ca899a616cbfc0a745f8e8b07bd5879c80a42c0db1bc69a607f1379befb173b4a01086e086e40b7'
