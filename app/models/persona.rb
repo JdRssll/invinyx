f8062d510618ecbcff54446e1f6376fd65a5d233
Jesus Rossell <jdr@jdr-VirtualBox.(none)>
2013-05-01 16:30:06 -0430

    Creado modelo de Persona

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -1,3 +1,7 @@
 class Persona < ActiveRecord::Base
   attr_accessible :nombre, :apellido, :direccion, :telefono, :email, :cargo
+  validates_presence_of :nombre, :apellido, :direccion, :telefono, :email, :cargo
+
+
+
 end
