a14799a65d4020fbb636b3d110d4d5f1c56aa880
gbrlmrllo <gbrlmrllo@gbrlmrllo-desktop.(none)>
2013-05-02 21:33:03 -0430

    Removiendo model Persona

--- app/models/persona.rb
+++
@@ -1,7 +0,0 @@
-class Persona < ActiveRecord::Base
-  attr_accessible :nombre, :apellido, :direccion, :telefono, :email, :cargo
-  validates_presence_of :nombre, :apellido, :direccion, :telefono, :email, :cargo
-
-
-
-end
