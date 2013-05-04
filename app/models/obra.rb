a1bbcf9a1099d80c58e480f7cfe782939496883f
gbrlmrllo <gbrlmrllo@gbrlmrllo-desktop.(none)>
2013-05-04 00:20:39 -0430

    Agregada validaciones para Obra (#1)

--- app/models/obra.rb
+++ app/models/obra.rb
@@ -2,6 +2,9 @@ class Obra < ActiveRecord::Base
   has_many :empleados, :dependent => :destroy
   attr_accessible :nombre
 
+  validates_presence_of :nombre
+  validates_length_of :nombre, :maximum => 45
+
   rails_admin do
   	create do
   		field :nombre
