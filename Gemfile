3aa04f604197b452a58c89269f902a464210d6a8
gbrlmrllo <gbrlmrllo@gmail.com>
2013-05-13 22:35:15 -0430

    Añadida y configurada gema Rmagick para estandarizar tamaño de las imagenes de articulos

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ end
 gem 'jquery-rails'
 gem 'devise'
 gem 'rails_admin'
+gem "rmagick"
 gem 'carrierwave'
 
 # To use ActiveModel has_secure_password
--- app/uploaders/foto_uploader.rb
+++ app/uploaders/foto_uploader.rb
@@ -1,7 +1,7 @@
 class FotoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
@@ -27,15 +27,15 @@ class FotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
+  # process :scale => [200, 200]
+  process :resize_to_fit => [150, 150]
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process :scale => [50, 50]
+  #  process :scale => [50, 50]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
