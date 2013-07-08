742d9e5c10e6c4a10c322b91333f6fe20ee4e1dd
gbrlmrllo <gbrlmrllo@gbrlmrllo-M2421.(none)>
2013-07-08 17:18:14 -0430

    Interfza del operador.. Luis

--- app/uploaders/foto_uploader.rb
+++ app/uploaders/foto_uploader.rb
@@ -28,15 +28,16 @@ class FotoUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 200]
-  process :resize_to_fit => [150, 150]
+  #process :resize_to_fit => [50, 50]
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #  process :scale => [50, 50]
-  # end
+  version :thumb do
+    #process :scale => [50, 50]
+    process :resize_to_fit => [50, 50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
