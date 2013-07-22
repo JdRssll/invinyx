4716a6e63734713fdda0432b6d160cc6ae4e278b
gbrlmrllo <gbrlmrllo@gbrlmrllo-M2421.(none)>
2013-07-22 10:37:34 -0430

    devoluciones

--- app/controllers/devolucions_controller.rb
+++ app/controllers/devolucions_controller.rb
@@ -1,10 +1,10 @@
-class DevolucionsController < ApplicationController
-  
-
-	before_filter :authenticate_user!
-
-
-	
+class DevolucionsController < ApplicationController 
+	before_filter :authenticate_user!	
   def index
+  end
+  def new
+  	@pedidos = Pedido.find(params[:id])
+  end
+  def create
   end
 end
