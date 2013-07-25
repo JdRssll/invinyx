24489f14711151394e11b1dbdfb6834a34d735b4
gbrlmrllo <gbrlmrllo@gbrlmrllo-M2421.(none)>
2013-07-25 12:54:51 -0430

    retoques al admin

--- app/controllers/devolucions_controller.rb
+++ app/controllers/devolucions_controller.rb
@@ -19,7 +19,7 @@ class DevolucionsController < ApplicationController
     @pedido = Pedido.find(@id)
     respond_to do |format|
       if @devolucion.save
-        format.html { redirect_to @devolucion, notice: 'Producto was successfully created.' }
+        format.html { redirect_to pedido_path(@id), notice: 'Producto was successfully created.' }
       else
         format.html { redirect_to new_pedido_devolucion_path(@id), :flash => { :alert => @devolucion.errors.messages } }
         format.json { render json: @devolucion.errors, status: :unprocessable_entity }
